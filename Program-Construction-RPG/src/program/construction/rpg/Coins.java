97e85f258d6023111b42e18ac349936807217eca
rynob <rynob@172.29.71.71>
2024-08-31 15:33:21 +1200

    no coins yet

--- Program-Construction-RPG/src/program/construction/rpg/Coins.java
+++ Program-Construction-RPG/src/program/construction/rpg/Coins.java
@@ -23,53 +23,53 @@ public class Coins {
     {
         this.Points = Points;
     }
-    public void generateCoins()
-    {
-        
-        Random rand = new Random();
-        int numberOfCoins = rand.nextInt(9)+1;//so zero wont ever appear
-        int coinsPlaced = 0;
-        while (coinsPlaced < numberOfCoins) {
-            int row = rand.nextInt(game.ROWS);
-            int col = rand.nextInt(game.COLS);
-            // Place a coin if the spot is empty
-            if (game.array[row][col] == '.') {
-                game.array[row][col] = 'C';
-                coinsPlaced++;
-            }
-        }
+   public void generateCoins()
+   {
+//        
+//        Random rand = new Random();
+//        int numberOfCoins = rand.nextInt(9)+1;//so zero wont ever appear
+//        int coinsPlaced = 0;
+//        while (coinsPlaced < numberOfCoins) {
+//            int row = rand.nextInt(game.ROWS);
+//            int col = rand.nextInt(game.COLS);
+//            // Place a coin if the spot is empty
+//            if (game.array[row][col] == '.') {
+//                game.array[row][col] = 'C';
+//                coinsPlaced++;
+//            }
+//        }
     }
-    
-    public void checkCoins(char move)
-    {
-            switch (move) {
-                case 'w'://move up
-                    if(game.array[game.currentRow--][game.currentCol] == 'C')
-                        {
-                            Points += 10;
-                        }
-                    break;
-                case 's':
-                        if(game.array[game.currentRow++][game.currentCol] == 'C')
-                        {
-                            Points += 10;
-                        }
-                    break;
-                case 'a':
-                         if(game.array[game.currentRow][game.currentCol--] == 'C')
-                        {
-                            Points += 10;
-                        }
-                    break;
-                case 'd':
-                         if(game.array[game.currentRow][game.currentCol++] == 'C')
-                        {
-                            Points += 10;
-                        }
-                    break;
-            }
+//    
+  public void checkCoins(char move)
+   {
+//            switch (move) {
+//                case 'w'://move up
+//                    if(game.array[game.currentRow--][game.currentCol] == 'C')
+//                        {
+//                            Points += 10;
+//                        }
+//                    break;
+//                case 's':
+//                        if(game.array[game.currentRow++][game.currentCol] == 'C')
+//                        {
+//                            Points += 10;
+//                        }
+//                    break;
+//                case 'a':
+//                         if(game.array[game.currentRow][game.currentCol--] == 'C')
+//                        {
+//                            Points += 10;
+//                        }
+//                    break;
+//                case 'd':
+//                         if(game.array[game.currentRow][game.currentCol++] == 'C')
+//                        {
+//                            Points += 10;
+//                        }
+//                    break;
+//            }
     }
-    
+//    
     
     
     
